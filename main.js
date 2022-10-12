1f2a43dc9763468dbd537c9e366164b848ba4a9c
shaurya2405 <92263901+shaurya2405@users.noreply.github.com>
2022-10-12 20:42:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+video="";
+
+function setup()
+{
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+
+function preload()
+{
+    video = createVideo('video.mp4');
+    video.hide();
+}
+
+function draw()
+{
+    image(video, 0, 0, 480, 380);
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("Status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded Successfully!");
+    Status = true;
+    video.loop();
+    video.speed(1);
+    video.volume();
+}
